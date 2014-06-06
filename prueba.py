56e1276b862497d500878f243fe6afcbe84b7f3b
David Vinazza <davidvinazza@gmail.com>
2014-06-06 13:21:37 -0300

    Saque las credenciales a un archivo distinto y lo puse en el git ignore

--- prueba.py
+++ prueba.py
@@ -1,28 +1,34 @@
+#!/usr/bin/python
+
 import urllib
 import urllib2
 import json
 import sys
 # There is an generic json-rpc implemantation in Python but it dose not work for me in this case so I worte Some functions
 
+urlBase = "http://encuestas.proyectokoala.org/index.php/admin/remotecontrol"
 
-       
+
 def get_session_key():
-    req = urllib2.Request(url='http://myurl/index.php/admin/remotecontrol',\
-                          data='{\"method\":\"get_session_key\",\"params\":{\"username\":\"admin\",\"password\":\"mypassword\"},\"id\":1}')
+    print "Request"
+    data='{\"method\":\"get_session_key\",\"params\":{\"username\":\"' + usuario +'\",\"password\":\"'+clave+'\"},\"id\":1}'
+    print data
+
+    req = urllib2.Request(url=urlBase,data=data)
     req.add_header('content-type', 'application/json')
     req.add_header('connection', 'Keep-Alive')
     try:
         f = urllib2.urlopen(req)
         myretun = f.read()
-        #print myretun
+        print myretun
         j=json.loads(myretun)
         return j['result']
     except :
         e = sys.exc_info()[0]
         print ( "<p>Error: %s</p>" % e )
-               
+
 def get_question_properties(skey,QuestionID):
-    req = urllib2.Request(url='http://myurl/index.php/admin/remotecontrol',\
+    req = urllib2.Request(url=urlBase,\
                           data='{\"method\":\"get_question_properties\",\"params\":{\"sSessionKey\":\"'+skey+'\",\"iQuestionID\":'+QuestionID+',\
 \"aQuestionSettings\":[\"gid\",\"type\",\"help\",\"language\",\"sid\",\"question_order\",\"question\",\"subquestions\"]},\"id\": 1}')
 
@@ -38,9 +44,9 @@ def get_question_properties(skey,QuestionID):
         e = sys.exc_info()[0]
         print ( "<p>Error: %s</p>" % e )
 
-               
+
 def release_session_key(relkey):
-    req = urllib2.Request(url='http://myurl/index.php/admin/remotecontrol',\
+    req = urllib2.Request(url=urlBase,\
                           data='{\"method\":\"release_session_key\",\"params\":{\"sSessionKey\":\"'+relkey+'\"},\"id\":1}')
     req.add_header('content-type', 'application/json')
     req.add_header('connection', 'Keep-Alive')
@@ -56,7 +62,7 @@ def release_session_key(relkey):
 
 
 def export_responses2(skey,sid):
-    req = urllib2.Request(url='http://myurl/index.php/admin/remotecontrol',\
+    req = urllib2.Request(url=urlBase,\
                           data='{\"method\":\"export_responses\",\"params\":{\"sSessionKey\":\"'+skey+'\",\"iSurveyID\":\"'+sid+'\",\
 \"DocumentType\":\"csv\",\"sLanguageCode\":\"de\",\"sHeadingType\":\"full\"},\
 "id\": 1}')
@@ -70,10 +76,11 @@ def export_responses2(skey,sid):
         return j['result']
     except :
         e = sys.exc_info()[0]
-        print ( "<p>Error: %s</p>" % e )               
+        print ( "<p>Error: %s</p>" % e )
 
-               
+
 mykey=get_session_key()
-print export_responses2(mykey,'566237').decode('base64')
-get_question_properties(mykey,'574')
+print "Obtuve",mykey
+#print export_responses2(mykey,'566237').decode('base64')
+#get_question_properties(mykey,'574')
 print release_session_key(mykey)
